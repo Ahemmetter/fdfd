c55ed5a38eda883c0ed27ffdb19b76156049a818
Ahemmetter <andreas.hemmetter@gmail.com>
2017-06-04 21:40:18 +0300

    added field visualization

--- fdfd1-pecpmc.py
+++ fdfd1-pecpmc.py
@@ -10,11 +10,11 @@ import matplotlib.pyplot as plt
 
 # --- Constants --- #
 
-n = 10                          # number of grid nodes
+n = 50                          # number of grid nodes
 dx = 1. / n                     # step size
-e = 1                           # permittivity (1 for vacuum)
-m = 1                           # permeability (1 for vacuum)
-l = 1 / (dx**2 * m * e)         # step factor for matrix
+e = 1.                           # permittivity (1 for vacuum)
+m = 1.                           # permeability (1 for vacuum)
+l = 1. / (dx**2 * m * e)         # step factor for matrix
 kstart = 2*np.pi                # wave vector target to find resonance wavelength
 modes = 6                       # number of modes that are being calculated
 
@@ -24,14 +24,14 @@ Mf = np.zeros((n, n))           # empty matrices
 Mb = np.zeros((n, n))
 
 for i in range(0, n):           # fills sparse matrices
-    Mf[i, i] = -1
+    Mf[i, i] = 1
     Mb[i, i] = 1
 
 for i in range(0, n-1):
-    Mf[i, i+1] = 1
+    Mf[i, i+1] = -1
     Mb[i+1, i] = -1
 
-M = l**2 * np.dot(Mb, Mf)       # dot multiplication to obtain M
+M = l * np.dot(Mb, Mf)              # dot multiplication to obtain M
 
 # --- Solver --- #
 
@@ -43,7 +43,12 @@ lam = 2*np.pi/np.real(kl)                                       # calculates wav
 
 # --- Plot commands --- #
 
-modearray = (2*np.linspace(0, modes-1, modes))+1                # simplification for analytical result
-plt.plot(np.linspace(0, modes-1, modes), lam, '-')              # plots numerical solution
-plt.plot(np.linspace(0, modes-1, modes), 4/modearray, '*')      # plots analytical solution
+plt.figure(1)
+modearray = (2*np.linspace(0, modes-1, modes))+1.             # simplification for analytical result
+plt.plot(np.linspace(1, modes, modes), lam, '-')              # plots numerical solution
+plt.plot(np.linspace(1, modes, modes), 4/modearray, '*')      # plots analytical solution
+
+f, ax = plt.subplots(6,1, sharex=True, sharey=True)
+for n in np.arange(v.shape[1]):
+    ax[n].plot(np.abs(v[:, vi[n]]), '-')
 plt.show()
